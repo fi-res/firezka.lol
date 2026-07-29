522fd4df792340c74e51ab6fbf1923df71351dbe
fi-res <nta16022013@gmail.com>
2026-07-29 18:44:47 +0600

    add chupep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import Response, FileResponse
 
 app = FastAPI()
 
@@ -6,4 +7,6 @@ app = FastAPI()
 def get():
 	return '6-7'
 
-
+@app.get("/chupep.jpg")
+async def chupep():
+    return FileResponse("chupep.jpg")
